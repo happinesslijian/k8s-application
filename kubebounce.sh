2fc083a2814a4deaa144047e4bbfcb18c5b2dd4d
lijian <lijian32@lenovo.com>
2020-12-21 17:33:05 +0800

    k8s命名空间中重启所有的pod

---
+++ kubebounce.sh
@@ -0,0 +1,8 @@
+deploys=`kubectl -n $1 get deployments | tail -n +2 | cut -d ' ' -f 1`
+for deploy in $deploys; do
+  kubectl -n $1 rollout restart deployments/$deploy
+done
+
+#参考链接
+#https://qvault.io/2020/10/26/how-to-restart-all-pods-in-a-kubernetes-namespace/?utm_sq=gl06dz1viv
+#https://github.com/lane-c-wagner/kubebounce
