8607d3acc23a30d72439689e09c1592d421d67e5
happinesslijian <47111417+happinesslijian@users.noreply.github.com>
2022-01-04 17:35:33 +0800

    Update kubebounce.sh

--- kubebounce.sh
+++ kubebounce.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+for n in $(kubectl get namespaces -o jsonpath={..metadata.name}); do
+   kubectl delete pod --all -n $n
+done
+---
+#!/bin/bash
 deploys=`kubectl -n $1 get deployments | tail -n +2 | cut -d ' ' -f 1`
 for deploy in $deploys; do
   kubectl -n $1 rollout restart deployments/$deploy
